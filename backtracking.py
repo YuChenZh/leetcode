4324cf21f58b9b2325bb0acfe61dd95fcadd6346
yujing-chen <46586588+yujing-chen@users.noreply.github.com>
2020-11-22 12:10:57 -0500

    Update backtracking.py

--- backtracking.py
+++ backtracking.py
@@ -1,4 +1,11 @@
 # 39 Combination Sum
+'''
+Given an array of distinct integers candidates and a target integer target, return a 
+list of all unique combinations of candidates where the chosen numbers sum to target. 
+You may return the combinations in any order.The same number may be chosen from candidates 
+an unlimited number of times. Two combinations are unique if the frequency of at least one 
+of the chosen numbers is different.
+'''
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         resu = []
@@ -14,6 +21,13 @@ class Solution:
             self.backtracking(nums[i:],target-nums[i],subset+[nums[i]],resu)
 
 # 40 Combination Sum II
+'''
+Given a collection of candidate numbers (candidates) and a target number (target), find 
+all unique combinations in candidates where the candidate numbers sum to target.
+Each number in candidates may only be used once in the combination.
+
+Note: The solution set must not contain duplicate combinations.
+'''
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:       
         res = []
@@ -31,44 +45,74 @@ class Solution:
             self.backtracking(res,i+1,subset+[nums[i]],nums,target-nums[i])
 
 # 46. Permutations
+'''
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+'''
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:           
-    
-    # Level0: []
-    # level1: [1]                  [2]              [3]
-    # level2: [1,2]    [1,3]       [2,1] [2,3]      [3,1] [3,2]
-    # level3: [1,2,3]  [1,3,2]     [2,1,3][2,3,1]   [3,1,2][3,2,1] 
-    
-    ## redo
-        visited = set()
-        res = []
-        self.backtracking(res,visited,[],nums)
-        return res       
-    
-    def backtracking(self,res,visited,subset,nums):
-        if len(subset) == len(nums):
-            res.append(subset)
+    res = []
+        self.dfs(nums, [], res)
+        return res
+
+    def dfs(self, nums, path, res):
+        if not nums:
+            res.append(path)
         for i in range(len(nums)):
-            if i not in visited:
-                visited.add(i)
-                self.backtracking(res,visited,subset+[nums[i]],nums)
-                visited.remove(i)
+            self.dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
                 
 # 47. Permutations II 
+'''
+Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+'''
 class Solution:
-    # need to improve, not time efficient
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        visited = set()
         res = []
-        self.backtracking(res,visited,[],nums)
-        return res       
-    
-    def backtracking(self,res,visited,subset,nums):
-        if len(subset) == len(nums):
-            if subset not in res:
-                res.append(subset)
+        nums.sort()
+        self.dfs(nums, [], res)
+        return res
+
+    def dfs(self, nums, path, res):
+        if not nums:
+            res.append(path)
         for i in range(len(nums)):
-            if i not in visited:
-                visited.add(i)
-                self.backtracking(res,visited,subset+[nums[i]],nums)
-                visited.remove(i)
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            self.dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
+            
+# 78. Subsets
+'''
+Given an integer array nums, return all possible subsets (the power set).
+'''
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+		# DFS
+        self.res = []
+        self.dfs(nums, 0, [])
+        return self.res
+    
+    def dfs(self, nums, pos, tmp):
+        self.res.append(tmp)
+        for i in range(pos, len(nums)):
+            self.dfs(nums, i+1, tmp+[nums[i]])
+            
+# 90. Subsets II  
+'''
+Given a collection of integers that might contain duplicates, nums, return all possible subsets (the power set).
+'''
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:                  
+  # solution 1: not space efficient           
+        resu = set()
+        # sort nums first to avoid duplicate subsets, e,g,.[1,2] and [2,1]
+        self.backtracking(resu,sorted(nums),[],0)
+        return list(resu)
+    
+    def backtracking(self,resu,nums,subset,idx):
+        # list is unhashble, convert subset to tuple then add to resu
+        resu.add(tuple(subset))  
+        for i in range(idx,len(nums)):
+            self.backtracking(resu,nums,subset+[nums[i]],i+1)
